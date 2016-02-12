a691ef9c51de1baaa402ccd0ff860f5cd49f600d
Adrian Perez <adrian.perez@orangeandbronze.com>
2016-02-12 13:27:29 +0800

    using post construct for better code

--- src/main/java/directory/watcher/Application.java
+++ src/main/java/directory/watcher/Application.java
@@ -1,5 +1,6 @@
 package directory.watcher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -7,13 +8,22 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.ApplicationContext;
 import directory.watcher.service.DirectoryWatcherService;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableConfigurationProperties
 public class Application {
 
+    @Autowired
+    private DirectoryWatcherService watcher;
+
     public static void main(String args[]) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        DirectoryWatcherService watcher = (DirectoryWatcherService) ctx.getBean("directoryWatcherService");
+
+    }
+
+    @PostConstruct
+    public void enableDirectoryWatcher(){
         watcher.watch();
     }
 }
